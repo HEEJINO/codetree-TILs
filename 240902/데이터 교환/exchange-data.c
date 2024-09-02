e29ed40c016ebdcabc14c93dd0557f677ab7ffbe
HEEJINO <106726976+HEEJINO@users.noreply.github.com>
2024-09-02 19:28:38 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240902/데이터 교환/exchange-data.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 6, c = 7;
+    int temp;
+
+    temp = b;
+    b = a;
+    a = c;
+    c = temp;
+
+    printf("%d\n%d\n%d", a, b, c);
+
+    return 0;
+}
