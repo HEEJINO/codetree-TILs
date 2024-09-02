8183f9287ce19a91689cad1b4ae3ec396f8cee27
HEEJINO <106726976+HEEJINO@users.noreply.github.com>
2024-09-02 11:03:40 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240902/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    double ft = 30.48; //1피트 = 30.48
+    double mi = 160934; //1마일 = 160934
+    printf("9.2ft = %.1lfcm\n", 9.2*ft);
+    printf("1.3mi = %.1lfcm", 1.3*mi);
+    return 0;
+}
