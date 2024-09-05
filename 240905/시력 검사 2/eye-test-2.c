43374efa04edbc314b3f35097e9bc0bc966045b3
HEEJINO <106726976+HEEJINO@users.noreply.github.com>
2024-09-05 10:25:23 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240905/시력 검사 2/eye-test-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    double a; //양쪽 눈 시력의 평균값 a
+    scanf("%lf", &a);
+    
+    if(a >= 1.0){
+        printf("High");
+    } else if(0.5 <= a < 1.0){
+        printf("Middle");
+    } else {
+        printf("Low");
+    }
+
+    return 0;
+}
