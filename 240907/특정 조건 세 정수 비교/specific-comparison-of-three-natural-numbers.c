657ebc0d947b21fa068bb4c7affade1907b3f414
HEEJINO <106726976+HEEJINO@users.noreply.github.com>
2024-09-07 02:48:35 +0900

    Title: 특정 조건 세 정수 비교, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240907/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
+++ 240907/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
@@ -6,16 +6,19 @@ int main() {
 
     //첫 번째 수가 세 정수의 최솟값과 일치한다면 1 OR 0을 출력
     //세 개의 수가 모두 같으면 1 OR 0을 출력
-
-    if (a == b || a == c)
-        printf("1 ");
-    else 
-        printf("0 ");
-
-    if (a == b && b == c)
+    if(a < b && a < c)
         printf("1");
-    else
+    else if (a <= b && a < c)
+        printf("1");
+    else if (a < b && a <= c)
+        printf("1");
+    else 
         printf("0");
+    
+    if (a == b && b == c)
+        printf(" 1");
+    else
+        printf(" 0");
 
     return 0;
 }
