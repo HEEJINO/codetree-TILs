00ed6da3426f7c1467baa2dd8b701ade68d51ad7
HEEJINO <106726976+HEEJINO@users.noreply.github.com>
2024-09-12 12:32:36 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/중앙값 구하기/find-the-median.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if((b>c && c>a) || (a>c && c>b))
+        printf("%d", c);
+    else if((b>a && c<a) || (c>a && a>b))
+        printf("%d", a);
+    else    
+        printf("%d", b);
+
+    return 0;
+}
