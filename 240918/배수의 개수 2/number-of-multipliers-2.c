88b65ab0059d58154a206e834b3d8166ebf13760
HEEJINO <106726976+HEEJINO@users.noreply.github.com>
2024-09-18 07:55:23 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240918/배수의 개수 2/number-of-multipliers-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, cnt=0;
+    for(int i=1; i<=10; i++){
+        scanf("%d\n", &a);
+        if(a % 2 == 1)
+            cnt++;
+    }
+    printf("%d", cnt);
+    return 0;
+}
