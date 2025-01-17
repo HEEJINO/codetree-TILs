cfde63064f6fcbf73518584cc5986f79f6c8971d
HEEJINO <106726976+HEEJINO@users.noreply.github.com>
2025-01-17 17:13:49 +0900

    Title: 별표 출력하기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/별표 출력하기 5/print-star-5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i=n; i>=1; i--){
+        for(int j=i; j>=1; j--){
+            //printf("~");
+            for (int k=i; k>=1; k--){
+                printf("*");
+            }
+            printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
